5a64caade5cdc80a817b64ae866cd6d63da37d9e
ronald112 <ronald11211@gmail.com>
2020-09-16 14:08:46 +0300

    beginners setup, example taglib

---
+++ utag/app/main.cpp
@@ -0,0 +1,8 @@
+#include "MiscHeaders.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    return dummy(argc, argv);
+}
---
+++ utag/app/src/MiscHeaders.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+
+// TAGLIB
+#include <tag.h>
+#include <fileref.h>
+#include <tpropertymap.h>
+
+int dummy(int argc, char *argv[]);
