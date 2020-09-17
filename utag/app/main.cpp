0ccabcb0a438c596402c025c2c30c9c0955f5f39
ronald112 <ronald11211@gmail.com>
2020-09-17 14:36:22 +0300

    add custom filesystem, works on ubuntu

--- utag/app/main.cpp
+++ utag/app/main.cpp
@@ -8,15 +8,15 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
     const regex pattern(R"((\.mp3|\.flac|\.waw|\.ogg)$)");
-    // for (const auto & entry : filesystem::directory_iterator(argv[1])) {
-    //     cmatch match;
-    //     if (entry.is_directory() == false
-    //         && regex_match(entry.path().extension().string().c_str(), match, pattern)
-    //         ) {
-    //             // std::cout << match.str(1) << std::endl;
-    //             std::cout << entry.path().filename().string() << std::endl;
-    //     }
-    // }
+    for (const auto & entry : fs::directory_iterator(argv[1])) {
+        cmatch match;
+        if (entry.is_directory() == false
+            && regex_match(entry.path().extension().string().c_str(), match, pattern)
+            ) {
+                // std::cout << match.str(1) << std::endl;
+                std::cout << entry.path().filename().string() << std::endl;
+        }
+    }
     TagLib::FileRef f(argv[1]);
 
     if(!f.isNull() && f.tag()) {
--- utag/app/src/MiscHeaders.h
+++ utag/app/src/MiscHeaders.h
@@ -8,8 +8,17 @@
 #include <string>
 #include <map>
 
-// #include <filesystem>
-// #include <expermental/filesystem>
+#if defined(__cplusplus) && __cplusplus >= 201703L && defined(__has_include)
+#if __has_include(<filesystem>)
+#define GHC_USE_STD_FS
+#include <filesystem>
+namespace fs = std::filesystem;
+#endif
+#endif
+#ifndef GHC_USE_STD_FS
+#include <ghc/filesystem.hpp>
+namespace fs = ghc::filesystem;
+#endif
 
 // TAGLIB
 #include <tag.h>
