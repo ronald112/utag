edf4d41f8e2acb6bf83141d5e7cd6eabe81ab13a
Yurii Burienkov <yburienkov@e3r5p6.unit.ua>
2020-09-19 18:44:16 +0300

    new gen unstable build

--- utag/app/src/TagEditor.cpp
+++ utag/app/src/TagEditor.cpp
@@ -1,19 +1,84 @@
 #include "TagEditor.h"
 
-TagEditor::TagEditor(QWidget *parent) :
-    QWidget(parent)
+TagEditor::TagEditor()
 {
-   button_ = new QPushButton(tr("Push Me!"));
-   textBrowser_ = new QTextBrowser();
+    QWidget *widget = new QWidget;
+    setCentralWidget(widget);
 
-   QGridLayout *mainLayout = new QGridLayout;
-   mainLayout->addWidget(button_,0,0);
-   mainLayout->addWidget(textBrowser_,1,0);
-   setLayout(mainLayout);
-   setWindowTitle(tr("Connecting buttons to processes.."));
+    QWidget *topFilter = new QWidget;
+    topFilter->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    infoLabel = new QLabel(tr("<i>Choose a menu option, or right-click to "
+                              "invoke a context menu</i>"));
+    infoLabel->setFrameStyle(QFrame::StyledPanel | QFrame::Sunken);
+    infoLabel->setAlignment(Qt::AlignCenter);
+
+    QWidget *bottomFilter = new QWidget;
+    bottomFilter->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->setContentsMargins(5, 5, 5, 5);
+    layout->addWidget(topFilter);
+    layout->addWidget(infoLabel);
+    layout->addWidget(bottomFilter);
+    widget->setLayout(layout);
+
+    createActions();
+    createMenus();
+
+    QString message = tr("A context menu is available by right-clicking");
+    statusBar()->showMessage(message);
+
+    setWindowTitle(tr("uTag"));
+    setMinimumSize(260, 400);
+    resize(320, 480);
 }
 
+void TagEditor::createActions() {
+    newAct = new QAction(tr("&New"), this);
+    newAct->setShortcuts(QKeySequence::New);
+    newAct->setStatusTip(tr("Create a new file"));
+    connect(newAct, &QAction::triggered, this, &TagEditor::newFile);
+
+    openAct = new QAction(tr("&Open"), this);
+    openAct->setShortcuts(QKeySequence::Open);
+    openAct->setStatusTip(tr("Open a file"));
+    connect(openAct, &QAction::triggered, this, &TagEditor::open);
+
+    saveAct = new QAction(tr("&Save"), this);
+    saveAct->setShortcuts(QKeySequence::Open);
+    saveAct->setStatusTip(tr("Save a file"));
+    connect(saveAct, &QAction::triggered, this, &TagEditor::save);
+
+    exitAct = new QAction(tr("&Save"), this);
+    exitAct->setShortcuts(QKeySequence::Open);
+    exitAct->setStatusTip(tr("Save a file"));
+    connect(exitAct, &QAction::triggered, this, &TagEditor::save);
+
+}
+
+
+void TagEditor::createMenus()
+{
+    fileMenu = menuBar->addMenu(tr("&File"));;
+    fileMenu->addAction(newAct);
+    fileMenu->addAction(openAct);
+    fileMenu->addAction(saveAct);
+    fileMenu->addSeparator();
+    fileMenu->addAction(exitAct);
+    
+}
+
+#ifndef QT_NO_CONTEXTMENU
+void TagEditor::contextMenuEvent(QContextMenuEvent *event)
+{
+    QMenu menu(this);
+    menu.addAction(cutAct);
+    menu.addAction(copyAct);
+    menu.addAction(pasteAct);
+    menu.exec(event->globalPos());
+}
+#endif // QT_NO_CONTEXTMENU
+
 TagEditor::~TagEditor() {
-    delete(button_);
-    delete(textBrowser_);
 }
--- utag/app/src/TagEditor.h
+++ utag/app/src/TagEditor.h
@@ -1,29 +1,61 @@
 #pragma once
 
-// #include <QtCore/QCoreApplication>
-// #include <QApplication>
-// #include <QtWidgets/QMainWindow>
-// #include <QtCore/QObject>
 #include <QtWidgets>
 #include <QtCore>
-// #include <QAction>
-// #include <QTextEdit>
-// #include <QPushButton>
-// #include <QTextBrowser>
-// #include <QGridLayout>
 
 class QPushButton;
 class QTextBrowser;
 
-class TagEditor : public QWidget
+class TagEditor : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit TagEditor(QWidget *parent = 0); //Constructor
+    TagEditor(); //Constructor
     ~TagEditor(); // Destructor
 
 private:
-   QPushButton* button_;
-   QTextBrowser* textBrowser_;
+    QPushButton* button_;
+    QTextBrowser* textBrowser_;
+    QFileSystemModel* dirmodel;
+    QTreeView* treeView;
+    QMenuBar *menuBar;
+
+protected:
+#ifndef QT_NO_CONTEXTMENU
+    void contextMenuEvent(QContextMenuEvent *event) override;
+#endif // QT_NO_CONTEXTMENU
+
+private:
+    void createActions();
+    void createMenus();
+    QMenu *fileMenu;
+    QMenu *editMenu;
+    QMenu *formatMenu;
+    QMenu *helpMenu;
+    QActionGroup *alignmentGroup;
+    QAction *newAct;
+    QAction *openAct;
+    QAction *saveAct;
+    QAction *exitAct;
+    QAction *undoAct;
+    QAction *redoAct;
+    QAction *cutAct;
+    QAction *copyAct;
+    QAction *pasteAct;
+    QAction *aboutAct;
+    QAction *aboutQtAct;
+    QLabel *infoLabel;
+
+private slots:
+    void newFile();
+    void open();
+    void save();
+    void undo();
+    void redo();
+    void cut();
+    void copy();
+    void paste();
+    void about();
+    void aboutQt();
 };
